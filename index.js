894491f9af1be0cab30dfafa68853b328f502e58
Raphael Ezeigwe <iraphael1308@gmail.com>
2024-02-08 16:16:22 +0100

    Add: create Ship class and method signatures

---
+++ index.js
@@ -0,0 +1,14 @@
+class Ship {
+    constructor(height, width) {
+        this.height = height
+        this.width = width
+        this.hits = 0
+        this.isSunk = false
+    }
+
+    create(cordinates) {}
+    hits() {}
+    isSunk() {}
+}
+
+module.exports = {Ship}
