f61a4c12144d486979d8b937407d8cbb944fa040
Raphael Ezeigwe <iraphael1308@gmail.com>
2024-02-11 13:54:23 +0100

    Feat: add GameBoard class

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ class Ship {
         this.isSunk = false;
     }
 
-    recieveAttack(point) {
-        point.isAttacked = true;
+    recieveAttack() {
         this.damage += 1;
         return this;
     }
@@ -32,4 +31,104 @@ class Point {
     }
 }
 
-module.exports = { Ship, Point };
+class GameBoard {
+    constructor(size = 10) {
+        this.board = [];
+        this.ships = [];
+        this.size = size;
+    }
+
+    getBoard() {
+        return this.board;
+    }
+
+    fill(pointClass) {
+        for (let i = 0; i < this.size; i++) {
+            let row = [];
+            for (let j = 0; j < this.size; j++) {
+                row.push(new pointClass(j, i));
+            }
+            this.board.push(row);
+        }
+        return this;
+    }
+
+    findSpace(col = 0, row = 0) {
+        if (row >= this.size - 1 || col >= this.size - 1) {
+            col = 0;
+            row = 0;
+        }
+
+        for (let i = row; i < this.size; i++) {
+            for (let j = col; j < this.size; j++) {
+                let point = this.board[i][j];
+                if (!point.isAttacked && !point.isOccupied) return point;
+            }
+        }
+
+        return null
+    }
+
+    canContain(space, size, isVertical) {
+        let spaceArray = [];
+        for (let i = 0; i < size; i++) {
+            if (isVertical) {
+                console.log(space.row, space.col)
+                spaceArray.push(this.board[space.row + i][space.col]);
+            } else {
+                spaceArray.push(this.board[space.row][space.col + i]);
+            }
+        }
+
+        if (spaceArray.every((point) => point && point.isOccupied === false)) return spaceArray;
+        return false;
+    }
+
+    place(ship) {
+        let col = 0;
+        let row = 0;
+        let canContain = null;
+
+        if (this.ships.length > 0) {
+            const [lastShip] = this.ships.slice(-1);
+            const point = lastShip.vertical ? lastShip.points[0] : lastShip.points.slice(-1)[0];
+            col = point.col + parseInt(Math.random() * 5);
+            row = point.row + parseInt(Math.random() * 5);
+        }
+
+        let space = this.findSpace(col, row);
+
+        while (space) {
+            canContain = this.canContain(space, ship.size, ship.vertical);
+            if (canContain) break;
+            space = this.findSpace(space.col + 1, space.row);
+        }
+
+        if (canContain) {
+            this.assignSpace(ship, canContain);
+            this.ships.push(ship);
+        }
+        return canContain;
+    }
+
+    assignSpace(ship, spaceArray) {
+        spaceArray.forEach((point) => (point.isOccupied = true));
+        ship.points = spaceArray;
+    }
+
+    recieveAttack(col, row) {
+        const point = this.board[row][col]
+        point.isAttacked = true
+
+        if (point.isOccupied) {
+            let [ship] = this.ships.filter((s) => s.points.include(point))
+            ship.recieveAttack()
+        }
+    }
+
+    isClear() {
+        return this.ships.every((ship) => ship.isSunk)
+    }
+}
+
+module.exports = { Ship, Point, GameBoard };
--- index.test.js
+++ index.test.js
@@ -2,16 +2,14 @@ const {Ship, Point} = require("./index.js")
 
 test("Ship registers hits", () => {
     const ship = new Ship(1, true)
-    const point = new Point(0, 0)
 
-    ship.recieveAttack(point)
+    ship.recieveAttack()
     expect(ship.damage).toBe(1)
 })
 
 test("Ship sinks", () => {
     const ship = new Ship(1)
-    const point = new Point(0, 0)
 
-    ship.recieveAttack(point)
+    ship.recieveAttack()
     expect(ship.status()).toBeTruthy()
 })
